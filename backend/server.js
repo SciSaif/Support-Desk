199f8a0e8f2c99069e95999e8c75292a5e2bb68c
saifullah rahman <saiflll284@gmail.com>
2022-02-02 06:36:41 +0530

    added the forms

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-console.log("Server");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+const PORT = process.env.PORT || 5000;
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+
+//connect to database
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+//Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server Started on PORT ${PORT}`));
